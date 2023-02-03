8f0a1692a53af033cf7258f66dd3e9459ded7ec5
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2023-02-03 15:16:28 +0300

    Add validation to the title attribute for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   after_save :update_posts_counter
+  after_initialize :set_comments_and_likes_counters
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, :likes_counter, numericality: true, comparison: { greater_than_or_equal_to: 0 }
 
   def update_posts_counter
     User.find(user_id).increment!(:posts_counter)
@@ -10,5 +14,12 @@ class Post < ApplicationRecord
 
   def recent_five_comments
     Comment.where(post_id: id).order(updated_at: :desc).limit(5)
+  end
+
+  private
+
+  def set_comments_and_likes_counters
+    self.likes_counter = 0 unless likes_counter
+    self.comments_counter = 0 unless comments_counter
   end
 end
