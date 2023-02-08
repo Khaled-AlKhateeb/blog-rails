42a41135256e7eb52d45917fd1ce368cdf51d8af
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2023-02-08 16:18:30 +0300

    Implement post view

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,5 +1,11 @@
 class PostController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @post = Post.where(author: @user)
+  end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    @comments = Comment.where(post: @post)
+  end
 end
