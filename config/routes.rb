93f4d51955feacb6941a4f718bc33265b4716cb3
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2023-02-08 10:25:32 +0300

    Fix branch issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root "user#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :user, only: [:index, :show] do
-    resources :post, only: [:index, :show]
-  end
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
